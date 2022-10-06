2cdd4ca7b6b1ada36d54752092744e62a371e9a5
David Ballantyne <david.hk.ballantyne@gmail.com>
2022-10-06 16:45:50 -0400

    Update and rename sharepoint.ps1 to Sharepoint/sharepoint.ps1

--- Sharepoint/sharepoint.ps1
+++ Sharepoint/sharepoint.ps1
@@ -1,11 +1,10 @@
 ﻿#THis script only identifies the sharepoint sites that have the Everyone Except Extended user.
 #run the #sharepoint-working.ps1 to run against 
 
-#Connect-SPOService -Url https://versaterminc-admin.sharepoint.com
+#Connect-SPOService -Url https://<site>.sharepoint.com
 clear
 
-$owner = 'vtm.migration@versaterm.com'
-
+$owner = '<Email of SharepointAdmin>'
 $csv = Import-Csv "C:\temp\sites.csv" -Delimiter "	"
 Add-Content C:/temp/enabled.txt "SiteURL"
 #Grab the Site URL from the csv file
@@ -40,26 +39,21 @@ $csv.SiteURL | ForEach-Object {
 {
     Add-Content C:/temp/enabled.txt "$siteURL"
     
-    #Write-Output '.Removed "Everyone except external Users" group from the site.'
-    #removes the Everyone Except External user from that site.
-    #Remove-SPOUser -Site $siteURL -Group "$site Members" -LoginName 'c:0-.f|rolemanager|spo-grid-all-users/67365ef4-bb04-4874-91f8-4d360af16390'
     
-    #Write-Output '.Added AllEmployeesVersaterm to the Site as a Site Member'
-    #Addes the All Employees Versaterm group to that site
-    #Add-SPOUser -Site $siteURL -Group "$site Members" -LoginName 'All Employees_Versaterm'
 }
 else{
     
-    #Add-Content C:/temp/enabled.txt "$site does NOT have the Everyone user enabled."
 }
     Write-Output '----------------------------------'
     
     Write-Output "Remove $owner as owner of $site"
-    #removes the VTMMigration as aditional admin from that site
+    #removes the owner as aditional admin from that site
     Set-SPOUser -site $siteURL -LoginName $owner -IsSiteCollectionAdmin $False
     
     Write-Output '_______________'
 }
+#Removes old file
 Remove-Item -Path "C:/Temp/enabled.csv"
+#converts to csv file for processing later.
 Move-Item -Path "C:/Temp/enabled.txt" -Destination "C:/Temp/enabled.csv"
 exit
